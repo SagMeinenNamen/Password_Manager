9436c10df49bf0d8cf8e19d3c092f4b0a85ce2cd
SagMeinenNamen <62218506+SagMeinenNamen@users.noreply.github.com>
2022-07-12 14:54:57 +0200

    Add files via upload

---
+++ Source/PasswordGenerator/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PasswordGenerator
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        // StartupUri="MainWindow.xaml"
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            string pathForPW = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\Manager";
+            Directory.CreateDirectory(pathForPW);
+            if (!File.Exists(pathForPW + "\\trowssap.txt"))
+            {
+                Window1 passwortEingeben = new Window1();
+                passwortEingeben.ShowDialog();
+                //passwortEingeben.Close();
+
+            }
+
+           
+
+
+        }
+
+    }
+}
